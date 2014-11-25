bb9c678848d639bbf85ef18d46d78eb0d7181cd9
ddmm-white <ddmm.white@gmail.com>
2014-11-25 22:39:37 +0300

    BackboneMongoTest
    Test

---
+++ BackboneMongo/BackboneMongo/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using BackboneMongo.Infrastructure;
+using BackboneMongo.Models;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace BackboneMongo.Controllers
+{
+	public class HomeController : Controller
+	{
+		public MongoService MongoService = new MongoService();
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult GetData( string name )
+		{
+			var collection = MongoService.GetCollection<Employee>( "Employee" );
+			var employees = collection.AsQueryable<Employee>();
+
+			if ( !string.IsNullOrEmpty( name ) )
+			{
+				employees = from e in employees
+							where e.firstName.ToLower().Contains( name ) || e.lastName.ToLower().Contains( name )
+							select e;
+			}
+
+			return Json( employees, JsonRequestBehavior.AllowGet );
+		}
+
+		[HttpPost]
+		public ActionResult UpdateData( Employee employee )
+		{
+			var collection = MongoService.GetCollection<Employee>( "Employee" );
+
+			var update = Update<Employee>.Set( x => x.firstName, employee.firstName )
+				.Set( x => x.lastName, employee.lastName )
+				.Set( x => x.age, employee.age )
+				.Set( x => x.description, employee.description );
+
+			collection.Update( Query.EQ( "_id", employee.Id ), update );
+
+			return Json( "{ Ok: true }" );
+		}
+
+		[HttpPost]
+		public ActionResult InsertData( Employee employee )
+		{
+			var collection = MongoService.GetCollection<Employee>( "Employee" );
+			collection.Insert( employee );
+			return Json( employee );
+		}
+
+		[HttpPost]
+		public ActionResult DeleteData( string mongoId )
+		{
+			var collection = MongoService.GetCollection<Employee>( "Employee" );
+			collection.Remove( Query.EQ( "_id", ObjectId.Parse( mongoId ) ) );
+			return Json( "{ Ok: true }" );
+		}
+	}
+}
---
+++ BackboneMongo/BackboneMongo/Infrastructure/MongoService.cs
@@ -0,0 +1,21 @@
+﻿using BackboneMongo.Models;
+using MongoDB.Driver;
+
+namespace BackboneMongo.Infrastructure
+{
+	public class MongoService
+	{
+		private const string ConnectionString = "mongodb://localhost";
+		private const string DbName = "[dbname]";
+
+		public MongoCollection GetCollection<T>( string collectionName ) 
+			where T : MongoDbCollection
+		{
+			var client = new MongoClient( ConnectionString );
+			var server = client.GetServer();
+			var database = server.GetDatabase( DbName );
+			var collection = database.GetCollection<T>( collectionName );
+			return collection;
+		}
+	}
+}
