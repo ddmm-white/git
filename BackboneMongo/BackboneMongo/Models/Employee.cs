f910c324887a09a502ad47abc8982624551f3c53
ddmm-white <ddmm.white@gmail.com>
2014-11-26 00:54:20 +0300

    BackboneMongoTest
    Test

---
+++ BackboneMongo/BackboneMongo/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace BackboneMongo.Models
+{
+	public class Employee : MongoDbCollection
+	{
+		public int age { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+
+		public string description { get; set; }
+	}
+}
---
+++ BackboneMongo/BackboneMongo/Models/MongoDbCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BackboneMongo.Models
+{
+	public class MongoDbCollection
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+
+		[DataMember]
+		public string MongoId
+		{
+			get
+			{
+				return Id.ToString();
+			}
+			set
+			{
+				Id = ObjectId.Parse( value );
+			}
+		}
+	}
+}
